eb3fd068c9ec681e973b4a714f7ba2b8749400a7
Gerrrard <71455352+Gerrrard@users.noreply.github.com>
2021-09-18 15:57:07 +0300

    Add files via upload

---
+++ Lab2/Utility.h
@@ -0,0 +1,24 @@
+#ifndef _UTILITY_H_
+#define _UTILITY_H_
+#include "ACurve.h"
+
+namespace Utility {
+	bool is_Int(const char* S);
+	bool is_Double(const char* S);
+	template <class V> int getNum(V& a, const char* msg);
+}
+
+namespace Menu {
+	void start(Lab2::ASpiral* spir);
+	int menu();
+	void menuExit(Lab2::ASpiral* spir);
+	void menuParametrInput(Lab2::ASpiral* spir);
+	void menuDistance(Lab2::ASpiral* spir);
+	void menuSectorArea(Lab2::ASpiral* spir);
+	void menuRingArea(Lab2::ASpiral* spir);
+	void menuTurnArea(Lab2::ASpiral* spir);
+	void menuLength(Lab2::ASpiral* spir);
+	void menuCurvatureRadius(Lab2::ASpiral* spir);
+}
+
+#endif
